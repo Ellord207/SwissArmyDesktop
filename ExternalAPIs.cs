7ad2fddbc024f0091f6140dc4c1b15afc1c79fb5
Ellord207 <nvelrod@gmail.com>
2018-07-13 16:33:05 -0500

    Initial commit

---
+++ ExternalAPIs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissArmyDesktop
+    {
+    public class ExternalAPIs
+        {
+
+        #region external Windows API
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        public const int SW_HIDE = 0;
+        public const int SW_SHOW = 5;
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetTopWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetNextWindow(IntPtr hWnd, uint wCmd);
+
+        [DllImport("user32.dll")]
+        public static extern Boolean SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        [DllImport("user32.dll")]
+        public static extern Boolean BringWindowToTop(IntPtr hWnd);
+        #endregion
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwissArmyDesktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //murder console window
+            var handle = ExternalAPIs.GetConsoleWindow();
+#if !DEBUG
+            // Hide
+            ExternalAPIs.ShowWindow(handle, ExternalAPIs.SW_HIDE);
+#else
+            // Show
+            ExternalAPIs.ShowWindow(handle, ExternalAPIs.SW_SHOW);
+#endif
+
+            Console.WriteLine("It starts.");
+            Application.Run(new TrayApp());
+
+            Console.WriteLine("Press Enter to Exit...");
+        }
+    }
+}
